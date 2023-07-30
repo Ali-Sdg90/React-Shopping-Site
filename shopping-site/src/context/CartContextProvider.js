eee5d32f4e5a7bf105f8e75d20a132938ee224b8
Ali Sadeghi <ali909392@gmail.com>
2023-07-30 21:13:53 +0330

    Progres files until vid 131

--- shopping-site/src/context/CartContextProvider.js
+++ shopping-site/src/context/CartContextProvider.js
@@ -35,6 +35,38 @@ const cartReducer = (state, action) => {
                 ...state,
                 selectedItems: [...newSelectedItems],
             };
+        case "INCREACE":
+            const indexI = state.selectedItems.findIndex(
+                (item) => item.id === action.payload.id
+            );
+            state.selectedItems[indexI].quantity++;
+            return {
+                ...state,
+            };
+        case "DECREASE":
+            const indexD = state.selectedItems.findIndex(
+                (item) => item.id === action.payload.id
+            );
+            state.selectedItems[indexD].quantity++;
+            return {
+                ...state,
+            };
+        case "CHECKOUT":
+            return {
+                selectedItems: [],
+                itemsCounter: 0,
+                total: 0,
+                checkout: true,
+            };
+        case "CLEAR":
+            return {
+                selectedItems: [],
+                itemsCounter: 0,
+                total: 0,
+                checkout: false,
+            };
+        default:
+            return state;
     }
 };
 
