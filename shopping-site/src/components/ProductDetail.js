e280a5356702f33b821bb7358e89d1ffcfc7595d
Ali Sadeghi <ali909392@gmail.com>
2023-07-30 14:56:05 +0330

    Add ProductDetail.js codes

--- shopping-site/src/components/ProductDetail.js
+++ shopping-site/src/components/ProductDetail.js
@@ -1,11 +1,39 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+
+import { ProductContexts } from "../context/ProductContextProvider";
 
 const ProductDetail = () => {
     const params = useParams();
+    const pageId = params.id;
+    const product = useContext(ProductContexts);
+    const { id, title, price, description, category, image, rating } =
+        product[pageId - 1];
     return (
-        <div>
-            <h1>Details - {params}</h1>
+        <div style={{ padding: "20px" }}>
+            <h1>Details - {pageId}</h1>
+            <br></br>
+            {console.log(
+                id,
+                title,
+                price,
+                description,
+                category,
+                image,
+                rating
+            )}
+            <img src={image} alt="product-img" style={{ width: "200px" }}></img>
+            <h2>
+                {id}. {title}
+            </h2>
+            <br></br>
+            <p>{description}</p>
+            <br></br>
+            <p>Category: {category}</p>
+            <p>Price: {price}$</p>
+            <p>Rating: {rating.rate}/5</p>
+            <br></br>
+            <Link to="/shopping-site/products">Go Back</Link>
         </div>
     );
 };
